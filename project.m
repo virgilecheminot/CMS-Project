87813eb7c759e688cb08c4e1b413529679a0086a
Virgile Cheminot <virgile.cheminot@gmail.com>
2025-01-11 12:16:59 +0100

    Finish Part 2 with simulink feddback system

--- project.m
+++ project.m
@@ -6,7 +6,7 @@ M = 1500;               % vehicle mass [kg]
 J_wheel = 1;            % moment of inertia of the wheel [kg*m^2]
 J_M = 0.05;             % moment of inertia of the motor [kg*m^2]
 alpha = deg2rad(5);     % slope of the road [degrees]
-R = 0.35;               % wheel radius [m]
+Rw = 0.35;               % wheel radius [m]
 r1 = 0.005;             % damping coefficient 1 [Nms/rad]
 r2 = 0.005;             % damping coefficient 2 [Nms/rad]
 k_t = Inf;              % torsional stiffness of the drive shaft [Nm/rad]
@@ -18,15 +18,15 @@ Cx = 0.3;               % drag coefficient [-]
 C_RR = 0.01;            % rolling coefficient [-]
 k_RR = 0.0002;          % rolling coefficient [s/m]
 
-m_star = J_M*(tau_1*tau_2/R)^2+4*J_wheel/(R^2)+M;
-r_star = (r1+r2)*(tau_2/R)^2;
+m_star = J_M*(tau_1*tau_2/Rw)^2+4*J_wheel/(Rw^2)+M;
+r_star = (r1+r2)*(tau_2/Rw)^2;
 fprintf('m*=%f ; r*=%f\n',m_star,r_star);
 
 %% Part 1 : Steady-state analysis
 v0 = 15;                % steady-state vehicle speed [m/s]
 v_w0 = 3.5;             % steady-state wind speed [m/s]
 
-ratio = R/(tau_2*tau_1);
+ratio = Rw/(tau_2*tau_1);
 F_D0 = 1/2*rho_air*Cx*A_front*(v0+v_w0)^2;
 F_R0 = M*g*C_RR*(1+k_RR*v0);
 P = M*g*sin(alpha);
@@ -96,12 +96,12 @@ fprintf('Bandwidth : %f\n',BW);
 
 % Bode diagrams of the open-loop transfer function
 figure;
-asymp(RG);
+bode(RG);
 title('Bode plot of the open-loop transfer function');
 
 % Bode diagrams of the closed-loop transfer function
 figure;
-asymp(L);
+bode(L);
 title('Bode plot of the closed-loop transfer function');
 
 % Bode diagrams of the closed-loop wind disturbance transfer function
@@ -110,5 +110,5 @@ G_D = tf(1, denG);
 Kc = numG;
 LD = K2 * feedback(G_D, Kc*R);
 figure;
-asymp(LD);
+bode(LD);
 title('Bode plot of the closed-loop wind disturbance transfer function');
